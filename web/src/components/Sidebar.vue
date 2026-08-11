00e8ab04171b554b342d4619a9b3ed57662533c3
Letter <nevermindzzt@gmail.com>
2026-08-11 09:00:44 +0000

    更新版本号至 v1.3.0

--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -75,7 +75,7 @@ const menuOptions = [
   },
 ]
 
-const appVersion = 'v1.2.7'
+const appVersion = 'v1.3.0'
 const projectUrl = 'https://github.com/NevermindZZT/zero-api'
 
 const activeKey = ref(route.path)
